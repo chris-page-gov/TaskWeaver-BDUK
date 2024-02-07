54f77c13d44df63b0a9ebc3166d7ba0a0ac64516
Chris Page <crpage@msn.com>
2024-02-07 06:51:10 +0000

    Switch to Ollama test Update taskweaver_config.json and taskweaver_config_example.json with new API key and additional settings

---
+++ project/taskweaver_config_check.py
@@ -0,0 +1,20 @@
+import json
+
+def get_allowed_modules():
+    with open('./project/taskweaver_config.json', 'r') as f:
+        config = json.load(f)
+    return config['code_interpreter']['allowed_modules']
+
+allowed_modules = get_allowed_modules()
+
+# Check if the modules are available
+unavailable_modules = []  # Create an empty list to store not available modules
+
+for module in allowed_modules:
+    try:
+        __import__(module)
+        print(f'"{module}",')
+    except ImportError:
+        unavailable_modules.append(module)  # Add not available module to the list
+
+print(f"\n\nUnavailble modules:\n {unavailable_modules}\n")
